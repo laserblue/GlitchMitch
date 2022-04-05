1bf5da8d768f21d8980a0e62c84f29b20521500d
laserblue <spectramollisol@gmail.com>
2022-04-05 14:09:49 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+/* change to scene class names
+import BootScene from './BootScene.js';
+import PreloadScene from './PreloadScene.js';
+import TitleScene from './TitleScene.js';
+import InstructionScene from './InstructionScene.js';
+import GameScene from './GameScene.js';
+import EndScene from './EndScene.js';
+
+// Load our scenes
+var bootScene = new BootScene();
+var preloadScene = new PreloadScene();
+var titleScene = new TitleScene();
+var instructionScene = new InstructionScene();
+var gameScene = new GameScene();
+var endScene = new EndScene();
+*/
     var config = {
         type: Phaser.AUTO,
         width: 800,
@@ -15,7 +31,20 @@
     };
 
     var game = new Phaser.Game(config);
+/*
+// load scenes
+game.scene.add('BootScene', bootScene);
+game.scene.add('PreloadScene', preloadScene);
+game.scene.add('TitleScene', titleScene);
+game.scene.add('InstructionScene', instructionScene);
+game.scene.add("GameScene", gameScene);
+game.scene.add("EndScene", endScene);
 
+
+// start title
+game.scene.start('BootScene');
+*/
+// remove preload and create
     function preload ()
     {
     //    this.load.image('background', 'Assets/Maize_Maze_at_Metton_-_geograph.org.uk_-_24223.jpg');
