41ebb33848b5d1f4600dfe6e31bdf28308050a38
laserblue <spectramollisol@gmail.com>
2022-04-05 14:39:39 -0600

    Create BootScene.js

---
+++ BootScene.js
@@ -0,0 +1,10 @@
+/*
+import BootScene from './BootScene.js'; // check capitalization for all imported file names
+import MenuScene from './MenuScene.js';
+import OptionsScene from './OptionsScene.js';
+import SceneMain from './sceneMain.js';
+import GameScene from './GameScene.js';
+import FSMScene from './FsmScene.js';
+import GameOverScene from './GameOverScene.js';
+import CreditsScene from './CreditsScene.js';
+*/
