a20cfe7d3b916a5b9f08eb367c765be3007d2709
laserblue <spectramollisol@gmail.com>
2022-04-05 13:43:34 -0600

    Create main.js
    added modular code for main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+    var config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 200 }
+            }
+        },
+        scene: {
+            preload: preload,
+            create: create
+        }
+    };
+
+    var game = new Phaser.Game(config);
+
+    function preload ()
+    {
+    //    this.load.image('background', 'Assets/Maize_Maze_at_Metton_-_geograph.org.uk_-_24223.jpg');
+        this.load.image('goddess', 'Assets/Relief_with_Maize_Goddess_(Chicomecóatl).jpg');
+    }
+
+    function create ()
+    {
+        this.add.image(400, 300, 'goddess');
+
+    }
