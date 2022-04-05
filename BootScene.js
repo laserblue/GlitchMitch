a6df6a63d321d6fb1488183bf96f9e2766dacb87
laserblue <spectramollisol@gmail.com>
2022-04-05 14:44:55 -0600

    Update BootScene.js

--- BootScene.js
+++ BootScene.js
@@ -1,4 +1,4 @@
-/*
+/* added for remembering names of scenes - remove later
 import BootScene from './BootScene.js'; // check capitalization for all imported file names
 import MenuScene from './MenuScene.js';
 import OptionsScene from './OptionsScene.js';
